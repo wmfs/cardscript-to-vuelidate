d868ef940d5ef597a7a4e3747711e65cc0f18f3e
reecebrend <reece.brend@wmfs.net>
2019-04-16 10:33:24 +0100

    test(): update test expects

--- test/tests.js
+++ test/tests.js
@@ -7,8 +7,9 @@ const { complex } = require('@wmfs/cardscript-examples')
 describe('Test the vuelidate converter', function () {
   it('convert the complex form', () => {
     const vuelidate = converter(complex)
-    expect(vuelidate.firstName.required).to.be.a('function')
-    expect(vuelidate.firstName.minLength).to.be.a('function')
-    expect(vuelidate.firstName.maxLength).to.be.a('function')
+    console.log('>>> ', vuelidate)
+    expect(vuelidate.name.required).to.be.a('function')
+    expect(vuelidate.name.minLength).to.be.a('function')
+    expect(vuelidate.name.maxLength).to.be.a('function')
   })
 })
