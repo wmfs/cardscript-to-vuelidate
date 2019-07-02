1ac9fa0471406e6d7c75cd40302100346f80e633
Meena.Hoda <meena.hoda@wmfs.net>
2019-07-02 14:48:10 +0100

    test: use specific example for this package

--- test/tests.js
+++ test/tests.js
@@ -2,14 +2,66 @@
 
 const converter = require('./../lib')
 const expect = require('chai').expect
-const { complex } = require('@wmfs/cardscript-examples')
 
-describe('Test the vuelidate converter', function () {
-  it('convert the complex form', () => {
-    const vuelidate = converter(complex)
-    console.log('>>> ', vuelidate)
-    expect(vuelidate.name.required).to.be.a('function')
-    expect(vuelidate.name.minLength).to.be.a('function')
-    expect(vuelidate.name.maxLength).to.be.a('function')
+const FORM = {
+  type: 'AdaptiveCard',
+  $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+  version: '1.0',
+  body: [
+    {
+      id: 'numberMin10Max50',
+      type: 'Input.Number',
+      validation: {
+        minimum: 10,
+        maximum: 50
+      }
+    },
+    {
+      id: 'textMinLen5MaxLen50',
+      type: 'Input.Text',
+      validation: {
+        minLength: 5,
+        maxLength: 50
+      }
+    },
+    {
+      id: 'textRequired',
+      type: 'Input.Text',
+      validation: {
+        required: true
+      }
+    },
+    {
+      id: 'textRequiredIf',
+      type: 'Input.Text',
+      validation: {
+        requiredIf: `data.pokemon = 'Psyduck' || data.pokemon = 'Pikachu'`
+      }
+    },
+    {
+      id: 'textEmail',
+      type: 'Input.Text',
+      validation: {
+        email: true
+      }
+    }
+  ]
+}
+
+describe('Test the vuelidate converter', () => {
+  it('convert a form using validations', () => {
+    const vuelidate = converter(FORM)
+
+    expect(vuelidate.numberMin10Max50.minValue).to.be.a('function')
+    expect(vuelidate.numberMin10Max50.maxValue).to.be.a('function')
+
+    expect(vuelidate.textMinLen5MaxLen50.minLength).to.be.a('function')
+    expect(vuelidate.textMinLen5MaxLen50.maxLength).to.be.a('function')
+
+    expect(vuelidate.textRequired.required).to.be.a('function')
+
+    expect(vuelidate.textRequiredIf.requiredIf).to.be.a('function')
+
+    // expect(vuelidate.textEmail.email).to.be.a('function')
   })
 })
